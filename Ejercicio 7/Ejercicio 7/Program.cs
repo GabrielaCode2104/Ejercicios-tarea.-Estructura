df180dfc65c4cb0c970d8384f2649a850b182b06
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 15:03:01 -0500

    Ejercicio 7

---
+++ Ejercicio 7/Ejercicio 7/Program.cs
@@ -0,0 +1,19 @@
+﻿//7) Calcula la suma de los números pares en un rango especificado por el usuario.
+Console.WriteLine("INGERSE EL NUMERO INICIAL DEL RANGO: ");
+int numInicio = int.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE EL NUMERO FINAL DEL RANGO: ");
+int numFinal = int.Parse(Console.ReadLine());
+
+int sumaPares = 0;
+for (int i = numInicio; i <= numFinal; i++)
+
+{
+    if (i % 2 == 0)
+    {
+        sumaPares += i;
+    }
+}
+
+Console.WriteLine($"La suma de los números pares en el rango es: {sumaPares} ");
+Console.ReadLine();
+
