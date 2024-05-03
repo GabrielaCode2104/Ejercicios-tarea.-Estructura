a732885ff0f54db71ad8cacee33fab9fe93714ec
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 18:10:43 -0500

    Ejercicio 13

---
+++ Ejercicio 13/Ejercicio 13/Program.cs
@@ -0,0 +1,10 @@
+﻿// 13) Crea un programa que genere la tabla de multiplicar de un número ingresado por el usuario. 
+Console.WriteLine("INGRESE UN NÚMERO: ");
+int num1 = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 12; i++)
+{
+    int resultado = num1 * i;   
+    Console.WriteLine(num1 + " x " +  i + " = " + resultado);
+}
+Console.ReadLine();
