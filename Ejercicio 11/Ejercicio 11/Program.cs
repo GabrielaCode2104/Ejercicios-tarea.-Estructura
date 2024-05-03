e0d83bbfafdbb116a8d9beb3ce5e1970cb35c4bd
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 17:46:53 -0500

    Ejercicios

---
+++ Ejercicio 11/Ejercicio 11/Program.cs
@@ -0,0 +1,11 @@
+﻿//Ordena una lista de números ingresados por el usuario de menor a mayor. 
+
+
+Console.WriteLine("Ingrese una lista de números separados por espacios: ");
+string[] numeros = Console.ReadLine().Split(' ');
+
+
+
+
+
+
---
+++ Ejercicio 12/Ejercicio 12/Program.cs
@@ -0,0 +1,34 @@
+﻿// 12) Verifica si una palabra ingresada por el usuario es un palíndromo.
+Console.WriteLine("Ingresa una palabra: ");
+string palabra = Console.ReadLine().ToLower();
+
+bool esPalindromo = EsPalindromo(palabra);
+
+if (esPalindromo)
+{
+    Console.WriteLine("Es un palindromo.");
+
+}
+else
+{
+    Console.WriteLine("No es un palindromo");
+}
+Console.ReadLine();
+
+static bool EsPalindromo(string palabra)
+{
+    int i = 0;
+    int j = palabra.Length - 1;
+
+    while (i < j)
+    {
+        if (palabra[i] != palabra[j])
+        {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+} 
+
