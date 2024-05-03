3183da408c24d022e8f16ed685a4a49ecb1ad16a
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 11:35:51 -0500

    Ejercicio

---
+++ ejercicio 1/ejercicio 1/Program.cs
@@ -0,0 +1,32 @@
+﻿//1) Realiza la suma, resta, multiplicación y división de dos números ingresados por el usuario. 
+
+double n, n2, suma, resta, multiplicacion, division;
+Console.WriteLine("Ingrese un número: ");
+n = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese un número: ");
+n2 = int.Parse(Console.ReadLine());
+
+
+// RESOLUCION
+suma = n + n2;
+resta = n - n2;
+multiplicacion = n * n2;
+
+if (n2 != 0)
+{
+    division = n / n2;
+}
+else
+{
+    Console.WriteLine("No se puede dividir entre cero");
+    division = 0;
+}
+
+//RESULTADOS
+
+Console.WriteLine($"La suma es:  {suma}");
+Console.WriteLine($"La resta es: {resta}");
+Console.WriteLine($"La multiplicación es: {multiplicacion}");
+Console.WriteLine($"La división es: {Math.Round(division, 2)}");
+
+Console.ReadLine();
