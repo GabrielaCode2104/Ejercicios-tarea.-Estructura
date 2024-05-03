deb9ab07f5fc44f93229b51cd76860da877caf24
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 13:10:33 -0500

    Ejercicio 03

---
+++ Ejercicio 3/Ejercicio 3/Program.cs
@@ -0,0 +1,19 @@
+﻿// 03 Pide la base y la altura de un triángulo al usuario y calcula su área. 
+
+double baseTri1, altTri1, areaTri;
+
+Console.WriteLine("Escriba la base del triangulo: ");
+baseTri1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Escriba la altura del triangulo: ");
+altTri1 = double.Parse(Console.ReadLine());
+
+//Resolución
+
+areaTri = (baseTri1 * altTri1) / 2;
+
+//Resultado
+
+Console.WriteLine($"El área del triangulo es: {areaTri}");
+
+Console.ReadLine();
+
