a64d215379d593ae1a28bf6d168189c72181c457
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 13:56:55 -0500

    Ejercicio 4

---
+++ Ejercicio 04/Ejercicio 04/Program.cs
@@ -0,0 +1,26 @@
+﻿// 4) Crea una función que calcule la factorial de un número. 
+ 
+//DECLARACIÓN
+
+Console.WriteLine("Ingrese un número entero: ");
+int n = int.Parse(Console.ReadLine());
+int factorial = 1;
+
+//RESOLUCION
+
+if (n == 0)
+{
+    factorial = 1;
+}
+else 
+{
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+}
+
+// RESULTADOS
+
+Console.WriteLine($"El factorial del número {n} es: {factorial}");
+Console.ReadLine();
