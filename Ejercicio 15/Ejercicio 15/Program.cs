07df47b682a84c851cb50230db01879f73fd3754
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 18:35:34 -0500

    Ejercicio 15

---
+++ Ejercicio 15/Ejercicio 15/Program.cs
@@ -0,0 +1,19 @@
+﻿// 15) Toma un número entero y calcula la suma de sus dígitos. 
+
+int num2 = 35243;
+
+int sumDig = CalcularSumDig(num2);
+
+Console.WriteLine($"La suma de los dígitos de {num2} es: {sumDig}");
+static int CalcularSumDig ( int num )
+{
+    int suma = 0;
+    while (num > 0)
+    {
+        int digito = num % 10;
+        suma += digito;
+        num /= 10;
+    }
+    return suma;
+        }
+Console.ReadLine();
