f0c9a4ba07973ec8c4bb2ce73af81c2e45a424bb
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 14:20:49 -0500

    Ejercicio 5

--- Ejercicio 5/Ejercicio 5/Program.cs
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//5) Verifica si un número ingresado por el usuario es primo o no. 
+
+//DECLARACIÓN
+
+Console.WriteLine("INGRESE UN NÚMERO ENTERO: ");
+int num1 = int.Parse(Console.ReadLine());
+
+// RESOLUCION
+
+if (num1 <= 0)
+{
+    Console.WriteLine("El número no es primo");
+}
+else
+{
+    bool es_primo = true;
+    for (int i = 2; i < num1; i++)
+    { 
+        if (num1 % i == 0) 
+        {
+            es_primo = false;
+            break;
+        }
+     }
+    if (es_primo)
+    {
+        Console.WriteLine("El número es primo");
+    }
+    else
+    {
+        Console.WriteLine("El número no es primo");
+    }
+}
+
+// RESULTADOS
+Console.ReadLine();
