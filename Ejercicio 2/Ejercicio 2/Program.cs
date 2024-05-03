076e43e22e98b5503ce56ce5ec78ff76bd1d9651
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 12:53:19 -0500

    Ejercicio 2

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,16 @@
+﻿//02 Solicita un número al usuario y determina si es par o impar
+Console.WriteLine("Ingrese un número: ");
+double num = double.Parse(Console.ReadLine());
+
+//Resolución
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Es par");
+}
+else
+{
+    Console.WriteLine("El número es impar");
+}
+
+Console.ReadLine();
