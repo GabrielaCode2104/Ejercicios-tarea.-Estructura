86fc6d86b4f81120273b67d15c282df9147cf387
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 14:32:18 -0500

    Ejercicio 6

---
+++ Ejercicio 6/Ejercicio 6/Program.cs
@@ -0,0 +1,14 @@
+﻿//6) Toma una cadena de texto y muestra su inversión. 
+Console.WriteLine("INGRESE UN TEXTO: ");
+string tex = Console.ReadLine();
+
+string texInvertido = " ";
+for (int i = tex.Length - 1; i >= 0; i--)
+{
+    texInvertido += tex[i];
+}
+
+Console.WriteLine("La cadena invertida es: " + texInvertido);
+
+Console.ReadLine();
+
