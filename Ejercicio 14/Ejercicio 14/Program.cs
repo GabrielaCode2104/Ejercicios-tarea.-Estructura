bd197f910b61d13c944c026fbf63e49da7d4eb45
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-03 18:24:53 -0500

    Ejercicio 14

---
+++ Ejercicio 14/Ejercicio 14/Program.cs
@@ -0,0 +1,13 @@
+﻿// 14) Pide el radio de un círculo al usuario y calcula su área. 
+
+double radio1, areaCirc;
+
+Console.WriteLine("Escriba el radio del circulo: ");
+radio1 = double.Parse(Console.ReadLine());
+
+areaCirc = Math.PI * Math.Pow(radio1, 2);
+
+Console.WriteLine($"El área del circulo es: {Math.Round(areaCirc,2)}");
+Console.ReadLine();
+
+
